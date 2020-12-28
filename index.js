983ff14fab861e59b1cea9cb13d4416eaba98104
TheWaildug <64437684+TheWaildug@users.noreply.github.com>
2020-12-27 20:45:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on("message", async message => {
     }else if(!data){
         prefix = "c!"
     }
-	console.log(prefix)
+	
     if(!message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).split(" ");
     const command = args.shift().toLowerCase();
