9e6096883629e43b1678a435583919758580fa99
DBowman121 <drewbowman121@gmail.com>
2020-12-28 04:00:14 +0000

    express

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
-var express = require('express');
-var app = express();
-
-
-module.exports = function(){
-        // set the port of our application
-        // process.env.PORT lets the port be set by Heroku
-        var port = process.env.PORT || 8080;
-        
-        // set the view engine to ejs
-        app.set('view engine', 'ejs');
-        
-        // make express look in the public directory for assets (css/js/img)
-        app.use(express.static(__dirname + '/public'));
-        
-        // set the home page route
-        app.get('/', function(req, res) {
-        
-            // ejs render automatically looks in the views folder
-            res.render('index');
-        });
-        
-        app.listen(port, function() {
-            console.log('Our app is running on http://localhost:' + port);
-        });
-        
+const express = require('express');
+const server = express();
+server.all('/', (req, res)=>{
+    res.send('Your bot is alive!')
+})
+function keepAlive(){
+    server.listen(3000, ()=>{console.log("Server is Ready!")});
 }
+module.exports = keepAlive;
