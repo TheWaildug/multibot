ded682bcea5c7c921815ddf0a57da4c7d4634b3c
d-bowman159 <64437684+d-bowman159@users.noreply.github.com>
2020-12-27 19:50:43 -0600

    port

--- index.js
+++ index.js
@@ -54,3 +54,6 @@ client.on("message", async message => {
     }
 })
 client.login(process.env.token)
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
