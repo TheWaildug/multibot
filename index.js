88f89c95d70496162d7787367bf8e3a2041c0b93
d-bowman159 <64437684+d-bowman159@users.noreply.github.com>
2020-12-29 21:55:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -199,7 +199,8 @@ client.on("message", async message => {
       
         
       }else if(command == "purge"){
-          console.log("purge")
+          console.log(`purge ${message.guild.id}`)
+          console.log(`purge ${message.member.id}`)
           var perm = false
           if(!message.member.hasPermission(`MANAGE_MESSAGES`)) return message.delete();
           const amount = args[0]
@@ -215,7 +216,9 @@ client.on("message", async message => {
                 if (amount < 1) return message.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
                 await message.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
                     message.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
-                )}).catch(console.error).then(() => message.channel.send(`Done!`))
+                )}).catch(console.error).then(() => message.channel.send(`Done!`)).then(msg => {
+                    msg.delete(2000)
+                })
                 return perm = false;
             }
         }); if(perm == false){
