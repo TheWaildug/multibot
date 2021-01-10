de7a66d66a9e5635ad15146c4b89e19fb5f9aae0
d-bowman159 <64437684+d-bowman159@users.noreply.github.com>
2021-01-10 15:53:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -377,8 +377,8 @@ client.on("message", async message => {
           const embed = new Discord.MessageEmbed()
           .setTitle(`Evaluation`)
           .setDescription(`Evaluated in *${Date.now() - message.createdTimestamp + " ms"}.*`)
-          .addField(`Input`,"```" + result + "```")
-          .addField(`Output`,"```" + evaled + "```")
+          .addField(`Input`,"```js" + result + "```")
+          .addField(`Output`,"```js" + evaled + "```")
           .setTimestamp()
           message.channel.send(`<@${message.member.id}>`,embed)
           }else if(command == "serverinfo"){
